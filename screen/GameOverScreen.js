14c06bfd1d5f5292c89d98817858ae6d6ca70e8c
Joel Brown <surfer_43@hotmail.com>
2020-05-19 15:53:53 -0400

    adding images locally and from web

--- screen/GameOverScreen.js
+++ screen/GameOverScreen.js
@@ -3,18 +3,33 @@ import {
     View,
     Text,
     Button,
+    Image,
     StyleSheet
 } from 'react-native';
 
-import Colors from '../constants/colors'
+import Bodytext from '../components/BodyText'
+
+import Colors from '../constants/colors';
+import DefaultStyles from '../constants/defaultStyles'
 
 const GameOverScreen = (props) => {
     const { guessCount, userNum } = props;
     return (
         <View style={styles.screen}>
-            <Text>Game Over</Text>
-            <Text>Your number was guessed in: {guessCount}</Text>
-            <Text>Your number was: {userNum}</Text>
+            <Text style={DefaultStyles.title}>Game Over</Text>
+            <View style={styles.imageContainer}>
+                <Image
+                // source={require('../assets/original.png')}
+                source={{
+                    uri:
+                    'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'
+                }}
+                style={styles.image}
+                resizeMode="cover"
+                />
+            </View>
+            <Bodytext>Your number was guessed in: {guessCount}</Bodytext>
+            <Bodytext>Your number was: {userNum}</Bodytext>
             <Button title="Restart" color={Colors.primary} onPress={props.restart} />
         </View>
     )
@@ -24,7 +39,21 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-    }
+        alignItems: 'center',
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+      },
+      image: {
+        width: '100%',
+        height: '100%'
+      }
 })
 
 export default GameOverScreen;
